b313901c933e99b387b26cc77758b34c58a280e5
Ben Marti <kareas1115@gmail.com>
2015-03-17 01:06:35 -0400

    Update mpower.py

--- mpower.py
+++ mpower.py
@@ -1,35 +1,48 @@
-#!/usr/bin/python
-
-#Script to control mPower Pro units from Ubiquity
-#William Marti
-
-import paramiko
-import sys
-import argparse
-
-#in this case im developing in vagrant, these will need to change
-username =  "vagrant";
-password =  "vagrant";
+def main(hostname, port, status):
 
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(
+
+  # display some lines
+
+
+  print hostname
+  print port
+  print status
+
+
+  username =  "vagrant";
+  password =  "vagrant";
+
+
+  ssh = paramiko.SSHClient()
+  ssh.set_missing_host_key_policy(
     paramiko.AutoAddPolicy())
-ssh.connect('127.0.0.1', username='vagrant',
+  ssh.connect('127.0.0.1', username='vagrant',
     password='vagrant')
 
 
-stdin, stdout, stderr = ssh.exec_command(
+  stdin, stdout, stderr = ssh.exec_command(
     "uptime")
 
+  stdin.flush()
 
-stdin.flush()
-
-
-data = stdout.read().splitlines()
-for line in data:
+  data = stdout.read().splitlines()
+  for line in data:
     print line
 
+  print "we got here"
 
-print "we got here"
 
+
+
+
+if __name__ == "__main__":
+  import sys
+  import paramiko
+
+  hostname = sys.argv[1]
+  port = sys.argv[2]
+  status = sys.argv[3]
+
+
+  main(hostname, port, status)

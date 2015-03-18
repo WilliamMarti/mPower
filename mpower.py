315a4d48ab1cbd60b9055e0862337aa6312c6e61
WilliamMarti <kareas1115@gmail.com>
2015-03-18 04:06:11 +0000

    .

--- mpower.py
+++ mpower.py
@@ -9,26 +9,29 @@ def main(hostname, port, status):
   print port
   print status
   
-
+  #ssh connection info
+  #vagrant for dev environment
   user =  "vagrant";
   password =  "vagrant";
   tcpport = 22;
 
+  #connect to a particular host
   ssh = paramiko.SSHClient()
   ssh.set_missing_host_key_policy(
     paramiko.AutoAddPolicy())
   ssh.connect(hostname,tcpport,user,password)
 
 
-
+  #'ports' are really just files with a 0 or 1 value
   port = '/dev/output' + port
 
+  #if we want to set to up
   if status == 'up':
     stdin, stdout, stderr = ssh.exec_command(
-      "echo 0 >" + port)
+      "echo 1 >" + port)
   else:
     stdin, stdout, stderr = ssh.exec_command(
-      "echo 1 >" + port)
+      "echo 0 >" + port)
   stdin.flush()
 
   data = stdout.read().splitlines()

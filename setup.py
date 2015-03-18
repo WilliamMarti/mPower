a8e340f553e3696af4f92627375f60f09bb6432f
WilliamMarti <kareas1115@gmail.com>
2015-03-18 15:41:27 +0000

    .

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name='mpower',
+      description='Scripted control of mPower Pro ports',
+      url='https://github.com/WilliamMarti/mPower',
+      
+      #Author Info
+      author='William Marti',
+      author_email='William.b.marti@gmail.com',
+      
+      license='GNU',
+      
+      version='1.0',
+
+      py_modules=['mpower'],
+      )
+

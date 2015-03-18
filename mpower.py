961cd4f27cfa5157c83868a78598b7bbbe23bc26
WilliamMarti <kareas1115@gmail.com>
2015-03-18 16:39:59 +0000

    .

--- mpower.py
+++ mpower.py
@@ -1,45 +1,48 @@
-def main(hostname, port, status):
+def turnOffPort(hostname,port):
 
+  user = "vagrant"
+  password =  "vagrant"
+  tcpport = 22
 
-
-  # display some lines
-  print hostname
-  print port
-  print status
-  
-  #ssh connection info
-  #vagrant for dev environment
-  user =  "vagrant";
-  password =  "vagrant";
-  tcpport = 22;
-
-  #connect to a particular host
   ssh = paramiko.SSHClient()
-  ssh.set_missing_host_key_policy(
-    paramiko.AutoAddPolicy())
+  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
   ssh.connect(hostname,tcpport,user,password)
 
-
-  #'ports' are really just files with a 0 or 1 value
   port = '/dev/output' + port
+  cmd = 'echo 0 > ' + port
+  print cmd
+  #using uptime for testing atm
+  stdin, stdout, stderr = ssh.exec_command("uptime")
+  #stdin, stdout, stderr = ssh.exec_command(cmd)
+  
+  print "Port " + port + " has been shutoff"
+
+def turnOnPort(hostname,port):
+
+  user = "vagrant"
+  password =  "vagrant"
+  tcpport = 22
+
+  ssh = paramiko.SSHClient()
+  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  ssh.connect(hostname,tcpport,user,password)
+
+  port = '/dev/output' + port
+  cmd = 'echo 1 > ' + port 
+
+  #using uptime for testing atm
+  stdin, stdout, stderr = ssh.exec_command("uptime")
+  #stdin, stdout, stderr = ssh.exec_command(cmd)
+  
+  print "Port " + port + " has been turned"
+  
+def main(hostname, port, status):
 
   #if we want to set to up
   if status == 'up':
-    stdin, stdout, stderr = ssh.exec_command(
-      "echo 1 >" + port)
+    turnOnPort(hostname, port)
   else:
-    stdin, stdout, stderr = ssh.exec_command(
-      "echo 0 >" + port)
-  stdin.flush()
-
-  data = stdout.read().splitlines()
-  for line in data:
-    print line
-
-
-
-
-
+    turnOffPort(hostname, port)
 
 if __name__ == "__main__":
   import sys
@@ -60,3 +63,7 @@ if __name__ == "__main__":
 
 
   main(hostname, port, status)
+
+
+
+
